32c0fa81479de43b87dedb60f8ae66bd325cc6ef
yonatan957 <yonatan22660@gmail.com>
2024-10-07 10:31:18 +0300

    create and connect empty router files

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
-console.log("app.js");
+const expreess = require("express");
+const dotenv = require("dotenv");
 
 
+const app = expreess();
+dotenv.config();
+
+app.use(expreess.json());
+app.use("/auth", require("./routes/authRouter"));
+app.use("/user", require("./routes/userRouter"));
+app.use("/greenEye", require("./routes/greenEyeRouter"));
+
+
+app.listen(process.env.PORT|| 3003, () => {
+    console.log(`server is running on port ${process.env.PORT}`);
+})
---
+++ src/routes/authRouter.js
@@ -0,0 +1,4 @@
+const expreess = require("express");
+const router = expreess.Router();
+
+module.exports = router;
